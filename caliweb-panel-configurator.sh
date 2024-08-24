ac3a280d3b26661ef9b399edaa8d8345c65152e6
Nick Derry <131623157+NickDerry2023@users.noreply.github.com>
2024-08-24 17:41:41 -0400

    [UPDATE] Included the addition of docker into Cali Panel Base Installer.

--- caliweb-panel-configurator.sh
+++ caliweb-panel-configurator.sh
@@ -13,6 +13,24 @@ function install_caliwebpanel() {
         echo "MariaDB is already installed."
     fi
 
+    # Install Docker and Set Docker up.
+
+    if [ ! -x "$(command -v docker)" ]; then
+        sudo apt-get update
+        sudo apt-get install ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+        echo \
+          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    else
+        echo "Docker is already installed."
+    fi
+
     # Install the latest version of PHP and PHP-FPM
     if [ ! -x "$(command -v php)" ] || [ ! -x "$(command -v php-fpm)" ]; then
         sudo apt update
